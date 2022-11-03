61596e824a4e149766792d20eccd79c455550318
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-03 15:03:22 -0400

    Añadido clase documento

---
+++ Documento.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace Books
+{
+    class Documento {
+        // Setteando parametros
+        public string autor = "";
+        public string titulo = "";
+        public string localizacion = "";
+        // Definiendo parametros
+        public string Autor
+        {
+            get {
+                return autor;
+            }
+            set {
+                autor = value;
+            }
+        }
+        public string Titulo
+        {
+            get {
+                return titulo;
+            }
+            set {
+                titulo = value;
+            }
+        }
+        public string Localizacion
+        {
+            get {
+                return localizacion;
+            }
+            set {
+                localizacion = value;
+            }
+        }
+        // Definicion de funciones
+        public void Mostrar() {
+            Console.WriteLine("El autor es " + autor + ", lo escribió en " + localizacion + " y el titulo de la obra es " + titulo);
+        }
+   }
+}
