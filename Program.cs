f9452a1f36f71d6605a734d3f8e30ef3c41cd672
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-03 13:43:58 -0400

    Init tarea 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Books
+{
+    class Program {
+        public static void Main(string[] args) 
+        {
+            Libros l1 = new Libros();
+            l1.Autor = "Jose";
+            l1.Localizacion = "Venecozuela";
+            l1.Titulo = "Sexo";
+            l1.Mostrar();
+        }
+   }
+}
