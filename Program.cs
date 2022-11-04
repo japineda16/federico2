31c0d1835e7247841618d36b10559fb75528b131
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-03 21:58:49 -0400

    Añadido comentarios nuevos

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace Books
     class Program {
         public static void Main(string[] args) 
         {
+            // Creando la instancia libros por primera vez, definiendo sus campos
             Libros l1 = new Libros();
             l1.Autor = "Jose";
             l1.Localizacion = "Venecozuela";
