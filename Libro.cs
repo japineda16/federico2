343eed756c37bf606749892361f449e5286860f2
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-03 22:03:19 -0400

    Añadido comentarios

--- Libro.cs
+++ Libro.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Books
 {
+    // Migrada todas las instrucciones a la clase documento
+    // Ahora la clase libro heredara todas las funcionalidades de la clase
+    // documento
     class Libros: Documento {
         // Se define el constructor de la clase y se le pasa los parametros del constructor.
         public Libros(string autor, string titulo, string localizacion) {
