eb4c66311f6f02b535fa2efd5422fee57b0f8356
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-03 21:48:24 -0400

    AÑADIDO 2DO CONSTRUCTOR

--- Libro.cs
+++ Libro.cs
@@ -4,7 +4,13 @@ namespace Books
 {
     class Libros: Documento {
         // Se define el constructor de la clase y se le pasa los parametros del constructor.
-        public Libros(string autor, string titulo, string localizacion) {
+        public Libros(string autor, string titulo) {
+
+        }
+        // Añadida el segundo constructor con el parametro por de fecto de localización
+        // Para evitar un error de compilado tuvo que eliminarse del primero constructor el parametro
+        // de localización.
+        public Libros(string titulo, string autor, string localizacion = "no detallada" ) {
 
         }
    }
