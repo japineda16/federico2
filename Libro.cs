c5934e3703bafb37cc545128f6da638cab065f9f
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-03 21:15:43 -0400

    comentarios

--- Libro.cs
+++ Libro.cs
@@ -3,6 +3,7 @@ using System;
 namespace Books
 {
     class Libros: Documento {
+        // Se define el constructor de la clase y se le pasa los parametros del constructor.
         public Libros(string autor, string titulo, string localizacion) {
 
         }
--- Program.cs
+++ Program.cs
@@ -5,10 +5,13 @@ namespace Books
     class Program {
         public static void Main(string[] args) 
         {
+            // se inicializa la clase y se le pasan los parametros de la clase Libro
             Libros l1 = new Libros("Jose", "<Titulo del libro>", "Venezuela");
+            // Se definen las propiedades de la clase libro.
             l1.Autor = "Jose";
             l1.Localizacion = "Venezuela";
             l1.Titulo = "<Titulo del libro>";
+            // se invoca la funcion mostrar de la case usando el objeto l1.
             l1.Mostrar();
         }
    }
