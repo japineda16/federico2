6463525c6895d28e51e8f981ed7bd841746f20ef
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-03 22:00:47 -0400

    Añadido comentarios

--- Libro.cs
+++ Libro.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Books
 {
+    // Migrada todas las instrucciones a la clase documento
+    // Ahora la clase libro heredara todas las funcionalidades de la clase
+    // documento
     class Libros: Documento {
         
    }
