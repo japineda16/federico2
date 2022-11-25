bbcd539a152efc43890299b9a15132771592f39f
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-11-25 09:15:12 -0400

    Actividad 1

--- Program.cs
+++ Program.cs
@@ -5,14 +5,34 @@ namespace Books
     class Program {
         public static void Main(string[] args) 
         {
+            agregarLibros();
+        }
+        public static void agregarLibros() {
             // se inicializa la clase y se le pasan los parametros de la clase Libro
-            Libros l1 = new Libros("Jose", "<Titulo del libro>", "Venezuela");
-            // Se definen las propiedades de la clase libro.
-            l1.Autor = "Jose";
-            l1.Localizacion = "Venezuela";
-            l1.Titulo = "<Titulo del libro>";
-            // se invoca la funcion mostrar de la case usando el objeto l1.
-            l1.Mostrar();
+            Libros[] l1 = new Libros[50];
+            Console.WriteLine("Para detener la iteración, deje sin rellenar el formulario en curso.");
+            // Iteracion para llenar arreglo de libros
+            for (int i = 0; i < 51; i++)
+            {
+                // Y se solicita los campos a conformar el arreglo de datos.
+                Console.WriteLine("Ingrese el titulo de la obra");
+                string titulo = Console.ReadLine();
+                Console.WriteLine("Ingrese la ubicación de la obra");
+                string ubicacion = Console.ReadLine();
+                Console.WriteLine("Ingrese el autor de la obra");
+                string autor = Console.ReadLine();
+                // Se valida el estado del formulario
+                // Es decir, si se cancela el formulario, o si se continua.
+                if (titulo.Length == 0 || ubicacion.Length == 0 || autor.Length == 0) {
+                    break;
+                }
+                // Se definen las propiedades de la clase.
+                l1[i] = new Libros(autor, titulo, ubicacion);
+                l1[i].Autor = autor;
+                l1[i].Localizacion = ubicacion;
+                l1[i].Titulo = titulo;
+                l1[i].Mostrar();
+            }
         }
    }
 }
